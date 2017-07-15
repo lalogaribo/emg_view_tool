c9875e4422e62ef172a51cf2fbfb190ed18ce8bf
lalogaribo <lalo.garibo90@gmail.com>
2017-07-15 18:33:08 -0500

    Dev tool initial commit

---
+++ lib/emg_view_tool.rb
@@ -0,0 +1,6 @@
+require "emg_view_tool/version"
+require "emg_view_tool/renderer"
+
+module EmgViewTool
+  # Your code goes here...
+end
---
+++ lib/emg_view_tool/version.rb
@@ -0,0 +1,3 @@
+module EmgViewTool
+  VERSION = "0.1.0"
+end
